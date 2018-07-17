89fad12286f23cc1f777df2f2a1f520e972438f7
Ahmad Kayyali <kayyali18@gmail.com>
2018-07-17 08:27:16 -0600

    Adjust button enabling and idea display

--- app.js
+++ app.js
@@ -16,13 +16,46 @@ $(document).ready(function () {
     `</article>`,
   ].join("\n"));
 
+  function checkInputs () {
+    if ($('.idea-title').val() && $('.textbox').val()) return false;
+    return true;
+  }
 
-  var counter = 0;
+  // function addIdea() {
+  //   $ideaCard = $([
+  //     `<article class="m-idea-card">`,
+  //       `<div class="idea-header flex-row">`,
+  //         `<h2>Example Idea 1</h2>`,
+  //         `<button class="delete-card svg" alt="Delete"></button>`,
+  //       `</div>`,
+  //       `<p class="idea-description">Tiramasu carrot fruitcake gingerbread man this is me woooo!</p>`,
+  //       `<div class="quality-buttons">`,
+  //         `<button class="upvote svg" role="button" aria-label="Upvote Idea"></button>`,
+  //         `<button class="downvote svg"></button>`,
+  //         `<p class="quality">quality: swill</p>`,
+  //       `</div>`,
+  //       `<hr>`,
+  //     `</article>`,
+  //   ].join("\n"));
+  //
+  //   $ideaContainer.append($ideaCard);
+  //
+  // }
+
 
   $('.save-btn').prop('disabled', true);
 
+  var counter = 0;
+  var $ideaContainer = $('.ideaContainer');
+  var $inputForm = $('.idea-form');
+
+
   $('.idea-title').on('keyup', function () {
-    $('.save-btn').prop('disabled', false);
+    $('.save-btn').prop('disabled', checkInputs);
+  })
+
+  $('.textbox').on('keyup', function () {
+    $('.save-btn').prop('disabled', checkInputs);
   })
 
 
@@ -32,6 +65,19 @@ $(document).ready(function () {
     counter++;
     console.log(counter);
     $('.idea-container').append($ideaCard);
+    $('.delete-card').on('click', function () {
+      $(this).parent().parent().hide(1000);
+    });
+    $inputForm.children().val('');
+    $('.save-btn').prop('disabled', checkInputs);
+
+  })
+
+  //delete
+
+  $('.delete-card').on('click', function (event) {
+    console.log($(this));
+    console.log('Hiiiiiiiii');
   })
 
 
